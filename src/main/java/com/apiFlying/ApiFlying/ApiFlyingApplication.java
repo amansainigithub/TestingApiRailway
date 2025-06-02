2a238a32495361eaf72b683be682d46e66f843ac
aman <amansaini1407@gmail.com>
2025-06-02 20:21:26 +0530

    first Commit

---
+++ src/main/java/com/apiFlying/ApiFlying/ApiFlyingApplication.java
@@ -0,0 +1,13 @@
+package com.apiFlying.ApiFlying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFlyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFlyingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiFlying/ApiFlying/ApiFlyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiFlying.ApiFlying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFlyingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
